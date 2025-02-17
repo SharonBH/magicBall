89ce2a8edc2f52517ab807fcb6e31f4354b46783
Sharon Ben Haim <37870168+SharonBH@users.noreply.github.com>
2025-02-17 09:51:17 +0200

    Update server.js

--- sports-club-backend/server.js
+++ sports-club-backend/server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const moment = require('moment');
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = 5000;
 app.use(cors());
 
 let monthlyExpenses = [];
